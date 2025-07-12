db3fe6c7db2899753885f74861215b08e250836e
jpsankari <JPSANKARI@HOTMAIL.COM>
2025-07-12 11:44:54 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,13 @@
 provider "aws" {
   region = "ap-southeast-1"
 }
+terraform {
+  required_version = ">= 1.6.0, < 1.9.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
